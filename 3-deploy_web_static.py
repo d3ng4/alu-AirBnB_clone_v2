5b6079010d8933a0a12ddf6de77ca5597ad0b6d3
karanidenis <d.waweru@alustudent.com>
2023-03-08 14:16:10 +0300

    deploying the archive

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,8 +3,8 @@
 script that creates and distributes an archive to your web servers,
 using the function deploy
 """
-from 1-pack_web_static import do_pack
-from 2-do_deploy_web_static import do_deploy
+do_pack = __import__('1-pack_web_static').do_pack
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
 def deploy():
     """
