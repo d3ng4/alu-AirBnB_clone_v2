81112a54401f97c1a3a1b1abe537611af105db9a
karanidenis <d.waweru@alustudent.com>
2023-03-08 13:24:18 +0300

    tar archives

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,10 +4,11 @@ creating a .tgz archive from contents of web_static
 folder using do_pack.
 """
 
+
 def do_pack():
     """create folder versions if it doesnt exist
     and pack files of web_static """
-    from datetime import datetime 
+    from datetime import datetime
     from fabric.api import local
     import os
     path = "versions"
@@ -16,12 +17,16 @@ def do_pack():
     now = datetime.now()
     date = now.strftime("%Y%m%d%H%M%S")
     command = "tar -cvzf versions/web_static_{}.tgz web_static".format(date)
-    local(command) # local("mkdir -p versions")
+    local(command)  # local("mkdir -p versions")
     new_file_name = "web_static_{}.tgz".format(date)
     # print("New file created: {}".format(new_file_name))
-    return new_file_name
     # local("tar -cvzf versions/web_static_{}.tgz web_static".format(date))
     # command = "tar -carvf web_static alu-AirBnB_clone_v2"
+    archive_path = "{}/{}".format(path, new_file_name)
+    if os.path.exists(archive_path):
+        return archive_path
+    
+    return None
 
 x = do_pack()
 print(x)
