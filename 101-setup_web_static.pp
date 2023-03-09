73d91b27f03a75cff430fd011c6a2a9c2655bb20
karanidenis <d.waweru@alustudent.com>
2023-03-09 17:05:35 +0300

    puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,4 +1,25 @@
 # using puppet to install and configure a web server
+
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://linktr.ee/firdaus_h_salim/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
 # install nginx if not already installed
 package {'nginx':
          ensure => 'installed',  
@@ -49,13 +70,18 @@ file {'/data/':
       mode => '0755',
       }
 # add nginx config
-file {'/etc/nginx/sites-available/default':
-      ensure => 'file',
-      content => template('web_static/'),
-      notify => Service['nginx'],
-      }
+# file {'/etc/nginx/sites-available/default':
+#       ensure => 'file',
+#       content => template('web_static/'),
+#       notify => Service['nginx'],
+#       }
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf
+}
 # restart nginx
-service {'nginx':
-         ensure => 'running',
-         enable => true,
-         }
+exec { 'restart nginx':
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d/',
+  require => File['/etc/nginx/sites-available/default'],
+}
