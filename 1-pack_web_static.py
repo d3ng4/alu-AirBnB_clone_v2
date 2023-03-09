6a313e3d5f735a7f44379b4f1365207359faa65d
karanidenis <d.waweru@alustudent.com>
2023-03-09 13:55:43 +0300

    tar archives

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,24 +7,25 @@ from datetime import datetime
 from fabric.api import local
 import os
 
+
 def do_pack():
     """create folder versions if it doesnt exist
     and pack files of web_static """
 
     try:
-      path = "versions"
-      # if not os.path.exists(path):
-      #     os.makedirs(path)
-          #  or
-      if os.path.isdir(path) is False:
-          local("mkdir versions")  # create folder versions
-      now = datetime.now()
-      date = now.strftime("%Y%m%d%H%M%S")
-      file_name = "versions/web_static_{}.tgz".format(date)
-      command = f"tar -cvzf {file_name} web_static"
-      local(command)
-      return file_name
-    except: 
+        path = "versions"
+        # if not os.path.exists(path):
+        #     os.makedirs(path)
+        #  or
+        if os.path.isdir(path) is False:
+            local("mkdir versions")  # create folder versions
+        now = datetime.now()
+        date = now.strftime("%Y%m%d%H%M%S")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        command = f"tar -cvzf {file_name} web_static"
+        local(command)
+        return file_name
+    except BaseException:
         return None
 
 # x = do_pack()
