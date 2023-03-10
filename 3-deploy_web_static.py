13b67ccd865b08cfcccc1f4bcdee60802ce33097
karanidenis <d.waweru@alustudent.com>
2023-03-10 11:57:37 +0300

    deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,9 +3,10 @@
 script that creates and distributes an archive to your web servers,
 using the function deploy
 """
+from fabric.api import env
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
-from fabric.api import env
+env.user = 'ubuntu'
 env.hosts = ['107.23.168.84', '52.90.109.65']  # list of web servers
 
 
