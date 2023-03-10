b2e007c7b2801d68f2baec9043ccdfdfab4299d6
karanidenis <d.waweru@alustudent.com>
2023-03-10 12:20:01 +0300

    cleaning up

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -6,17 +6,6 @@ using the function do_clean
 from fabric.api import local, run, env
 env.hosts = ['107.23.168.84', '52.90.109.65']  # list of web servers
 
-
-# def do_clean(number=0):
-#     """
-#     function that deletes out-of-date archives
-#     """
-#     number = int(number)
-#     if number == 0 or number == 1:
-#         local("ls -t versions | tail -n +2 | xargs rm -f")
-#     else:
-#         local("ls -t versions | tail -n +{} | xargs rm -f".format(number + 1))
-#     # local("ls -t versions | tail -n +3 | xargs rm -f") # delete all but last 2
     
 def do_clean(number=0):
     """Deletes out-of-date archives"""
