354b69285a42731e16c9aed9a28d2967ae286e70
karanidenis <d.waweru@alustudent.com>
2023-03-14 13:15:47 +0300

    AirBNB

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -67,6 +67,6 @@ class FileStorage:
         else:
             return
 
-def close(self):
-    """call reload() method for deserializing the JSON file to objects"""
-    self.reload()
+    def close(self):
+        """call reload() method for deserializing the JSON file to objects"""
+        self.reload()
